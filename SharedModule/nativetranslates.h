1736f4f05e116c836d162f7c0840a4bc2b6de0fe
Andrey Zhuchkin <andrey.zhuchkin@rubius.com>
2018-07-27 16:27:33 +0700

    add TR define for QString

--- SharedModule/nativetranslates.h
+++ SharedModule/nativetranslates.h
@@ -2,11 +2,18 @@
 #define NATIVETRANSLATES_H
 
 #ifdef QT_C_TRANSLATES
+
 #include <QObject>
 #define TR_W(text) \
     QObject::tr(text).toStdWString()
+#define TR(text) \
+    QObject::tr(text)
+
 #else
+
 #define TR_W(text) L##text
+#define TR(text) text
+
 #endif
 
 #endif // TRANSLATES_H
