4085e6b9f58819bb1cc41b30adb46db7169c1d9a
HowCanidothis <des_av@mail.ru>
2018-11-13 16:21:17 +0700

    tr made static

--- SharedModule/nativetranslates.h
+++ SharedModule/nativetranslates.h
@@ -7,7 +7,7 @@
 
 #define Q_DECLARE_TR_FUNCTIONS_W(context) \
 protected: \
-    std::wstring tr(const char* text) const { return QCoreApplication::translate(#context, text, nullptr, -1).toStdWString(); }
+    static std::wstring tr(const char* text) { return QCoreApplication::translate(#context, text, nullptr, -1).toStdWString(); }
 
 #else
 
